0249ec385a0f9a116a6d9bad40286b88382d480b
hhwangkr <64496040+hhwangkr@users.noreply.github.com>
2020-05-09 11:38:42 +0100

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -8,25 +8,29 @@ def set_initialisation(optimisation_model, set_class):
     and initialise the set for the model
     """
     # set of monomer production plants
+    optimisation_model.G = pyo.Set(initialize = set_class.G,
+                         doc = 'customers', ordered = True)
+    
     optimisation_model.i = pyo.Set(initialize = set_class.i,
-                         doc = 'monomer production plants', ordered = True)
+                         doc = 'node1', ordered = True)
 
     optimisation_model.j = pyo.Set(initialize = set_class.j,
-                         doc = 'polymer plants', ordered = True)
+                         doc = 'node2', ordered = True)
 
-    optimisation_model.g = pyo.Set(initialize = set_class.g,
-                         doc = 'polymer with different grades', ordered = True)
+    optimisation_model.o = pyo.Set(initialize = set_class.o,
+                         doc = 'origins', ordered = True)
 
-    optimisation_model.t = pyo.Set(initialize = set_class.t,
-                         doc = 'time periods', ordered = True)
+    optimisation_model.d = pyo.Set(initialize = set_class.d,
+                         doc = 'destinations', ordered = True)
 
-    optimisation_model.m = pyo.Set(initialize = set_class.m,
-                         doc = 'materials: alkane/olefins', ordered = True)
+    optimisation_model.l = pyo.Set(initialize = set_class.L,
+                         doc = 'Locomotive type', ordered = True)
 
-    optimisation_model.c = pyo.Set(initialize = set_class.c,
-                         doc = 'customers', ordered = True)
+    optimisation_model.c = pyo.Set(initialize = set_class.C,
+                         doc = 'Container type', ordered = True)
 
-    optimisation_model.h = pyo.Set(initialize = set_class.h,
-                         doc = 'IHP', ordered = True)
+    optimisation_model.w = pyo.Set(initialize = set_class.W,
+                         doc = 'Wagon type', ordered = True)
 
-    
+    optimisation_model.t = pyo.Set(initialize = set_class.T,
+                         doc = 'time periods', ordered = True)    
