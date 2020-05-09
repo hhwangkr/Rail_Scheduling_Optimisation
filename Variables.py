60fe342f0d259e866014666671bd6b4b7f326ad5
hhwangkr <64496040+hhwangkr@users.noreply.github.com>
2020-05-09 12:50:47 +0100

    Define model variables

--- Variables.py
+++ Variables.py
@@ -8,29 +8,9 @@ def variable_initialisation(optimisation_model):
     # get the set of time period as a list
     hsetlist = list(optimisation_model.h)
 
-    def PObounds(optimisation_model, i, t):
+    def QCfx(optimisation_model, l, i, j, t):
         """
-        This function defines the bounds for PO(i)
-        """
-        #return (20, 30)
-        return (optimisation_model.p_min[i], optimisation_model.p_max[i])
-
-    def ICbounds(optimisation_model, m, t):
-        """
-        This function defines the bounds for IC
-        """
-        return (optimisation_model.IC_low[m], optimisation_model.IC_upper[m])
-
-    def IHbounds(optimisation_model, g, h, t):
-        """
-        This function defines the bounds for IH
-        """
-        return (optimisation_model.IH_low[g, h], None)
-        #return (optimisation_model.IH_low[g, h], None)
-
-
-    def QCfx(optimisation_model, c, g, h, t):
-        """This function fix the QC value of any customer/region pairs
+        This function fix the QC value of any customer/region pairs
         that is not feasible to zero
         """
         if optimisation_model.HC[h, c] != 1:
@@ -41,69 +21,47 @@ def variable_initialisation(optimisation_model):
             return None
 
 
-    optimisation_model.PM = pyo.Var(
-                            optimisation_model.i, optimisation_model.m,
-                            optimisation_model.t,
-                            within = pyo.NonNegativeReals,
-                            doc = 'amount of m consumed in unit i over t'
+    optimisation_model.x = pyo.Var(
+                            optimisation_model.l, optimisation_model.i,
+                            optimisation_model.j, optimisation_model.t,
+                            within = pyo.Binary,
+                            doc = 'locomotive (of type l) leaving i for j at period t'
+                            )
+    
+    optimisation_model.M = pyo.Var(
+                           optimisation_model.l, optimisation_model.i,
+                           optimisation_model.t,
+                           within = pyo.NonNegativeIntegers,
+                           doc = '# locomotives (of type l) stationed at i at period t'
+                           )
+
+    optimisation_model.WM = pyo.Var(
+                            optimisation_model.w, optimisation_model.i,
+                            optimisation_model.j, optimisation_model.t,
+                            within = pyo.NonNegativeIntegers,
+                            doc = '# Wagons (of type w) leaving i for j at period t'
                             )
 
-    optimisation_model.PO = pyo.Var(
+    optimisation_model.WS = pyo.Var(
+                            optimisation_model.w, optimisation_model.i,
+                            optimisation_model.t,
+                            within = pyo.NonNegativeIntegers,
+                            doc = '# Wagons (of type w) stationed at i at period t'
+                            )
+
+    optimisation_model.CM = pyo.Var(
+                            optimisation_model.c, optimisation_model.g,
+                            optimisation_model.o, optimisation_model.d,
+                            optimisation_model.i, optimisation_model.j,
+                            optimisation_model.t,
+                            within = pyo.NonNegativeIntegers,
+                            doc = '# Containers (of type c for customer g, to be delivered from o to d) leaving i for j at period t'
+                            )
+
+    optimisation_model.CS = pyo.Var(
+                            optimisation_model.c, optimisation_model.g,
+                            optimisation_model.o, optimisation_model.d,
                             optimisation_model.i, optimisation_model.t,
-                            within = pyo.NonNegativeReals,
-                            bounds = PObounds,
-                            doc = 'production amount of plant i in period t'
+                            within = pyo.NonNegativeIntegers,
+                            doc = '# Containers (of type c for customer g, to be delivered from o to d) staioned at i at period t'
                             )
-
-    optimisation_model.PP = pyo.Var(
-                            optimisation_model.g, optimisation_model.j,
-                            optimisation_model.t,
-                            within = pyo.NonNegativeReals,
-                            doc = 'amount of grade g produced in plant j in t'
-                            )
-
-    optimisation_model.IC = pyo.Var(
-                            optimisation_model.m, optimisation_model.t,
-                            within = pyo.NonNegativeReals, bounds = ICbounds,
-                            doc = 'plant inventory level of olefin o at t '
-                            )
-
-    optimisation_model.PU = pyo.Var(
-                            optimisation_model.m, optimisation_model.t,
-                            within = pyo.NonNegativeReals,
-                            doc = 'amount of olefin o purchased at period t'
-                            )
-
-    optimisation_model.S = pyo.Var(
-                           optimisation_model.m, optimisation_model.t,
-                           within = pyo.NonNegativeReals,
-                           doc = 'amount of olefin sold by export at period t'
-                           )
-
-
-    optimisation_model.Q = pyo.Var(
-                           optimisation_model.g, optimisation_model.h,
-                           optimisation_model.t, within = pyo.NonNegativeReals,
-                           doc = 'shipment of g from UAE to h at period t'
-                           )
-
-    optimisation_model.IH = pyo.Var(
-                            optimisation_model.g, optimisation_model.h,
-                            optimisation_model.t,
-                            within = pyo.NonNegativeReals,
-                            bounds = IHbounds, doc = \
-                            'inventory level of grade g at IHP h at period t'
-                            )
-
-    optimisation_model.QC = pyo.Var(
-                           optimisation_model.c, optimisation_model.g,
-                           optimisation_model.h, optimisation_model.t,
-                           within = pyo.NonNegativeReals,
-                           initialize = QCfx,
-                           doc = 'supply of g from h to customer c at period t'
-                           )
-    optimisation_model.Y = pyo.Var(
-                           optimisation_model.g, optimisation_model.j,
-                           optimisation_model.t, within = pyo.Binary,
-                           doc = 'supply of g from h to customer c at period t'
-                           )
