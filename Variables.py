5221520129d5af4f881f959f1a58e7b16c291c32
hhwangkr <64496040+hhwangkr@users.noreply.github.com>
2020-05-30 14:12:28 +0100

    Define model variables

--- Variables.py
+++ Variables.py
@@ -36,7 +36,7 @@ def variable_initialisation(optimisation_model):
 
     optimisation_model.CM = pyo.Var(
                             optimisation_model.c, optimisation_model.g,
-                            optimisation_model.o, optimisation_model.i,
+                            optimisation_model.d, optimisation_model.i,
                             optimisation_model.j, optimisation_model.t,
                             within = pyo.NonNegativeIntegers,
                             doc = '# Containers (of type c for customer g, to be delivered from o) leaving i for j at period t'
@@ -44,7 +44,7 @@ def variable_initialisation(optimisation_model):
 
     optimisation_model.CS = pyo.Var(
                             optimisation_model.c, optimisation_model.g,
-                            optimisation_model.o, optimisation_model.i,
+                            optimisation_model.d, optimisation_model.i,
                             optimisation_model.t,
                             within = pyo.NonNegativeIntegers,
                             doc = '# Containers (of type c for customer g, to be delivered from o) staioned at i at period t'
