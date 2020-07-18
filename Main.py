7e53619b3b26007528b5a2f677dd121ef9d29d83
hhwangkr <64496040+hhwangkr@users.noreply.github.com>
2020-07-18 14:02:22 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -27,72 +27,47 @@ def data_construction(file_name):
     t = [n for n in range(0,49)]
 
     set_input = faux.SetInput(g, i, j, o, d, l, c, w, t)
-
+    
+    # Maximum number of wagons on each service
+    WMAX = 30
+    
     # load single-dimension parameters of set 'i'
-    P = faux.read_par_from_excel('Pyomo_RSO_Parameter_Input.xlsx','1DPar_i', (2, 'A'), (6, 'B'), 1)
-    NC = faux.read_par_from_excel('Pyomo_RSO_Parameter_Input.xlsx','1DPar_i', (2, 'A'), (6, 'C'), 1)
-    NL = faux.read_par_from_excel('Pyomo_RSO_Parameter_Input.xlsx','1DPar_i', (2, 'A'), (6, 'D'), 1)
-    NW = faux.read_par_from_excel('Pyomo_RSO_Parameter_Input.xlsx','1DPar_i', (2, 'A'), (6, 'E'), 1)
+    P = faux.read_par_from_excel(Excel_file,'1DPar_i', (2, 'A'), (6, 'B'), 1)
+    NC = faux.read_par_from_excel(Excel_file,'1DPar_i', (2, 'A'), (6, 'C'), 1)
+    NL = faux.read_par_from_excel(Excel_file,'1DPar_i', (2, 'A'), (6, 'D'), 1)
+    NW = faux.read_par_from_excel(Excel_file,'1DPar_i', (2, 'A'), (6, 'E'), 1)
 
     # load asset ownership information
-    OL = faux.read_par_from_excel('Pyomo_RSO_Parameter_Input.xlsx','1DPar_OL', (2, 'A'), (4, 'B'), 1)
-    OW = faux.read_par_from_excel('Pyomo_RSO_Parameter_Input.xlsx','1DPar_OW', (2, 'A'), (4, 'B'), 1)
+    OL = faux.read_par_from_excel(Excel_file,'1DPar_OL', (2, 'A'), (4, 'B'), 1)
+    OW = faux.read_par_from_excel(Excel_file,'1DPar_OW', (2, 'A'), (4, 'B'), 1)
 
     # load journey time of feasible legs
-    LT = faux.read_par_from_excel(file_name,
-                'WarehousesShipping', (5, 'C'), (6, 'E'), (0, 1))
+    H = faux.read_par_from_excel(Excel_file,'2DPar_H', (2, 'A'), (18, 'C'), 2)
+    H = {key:val for key, val in H.items() if val != 0}
 
-    IH_low = faux.read_par_from_excel(file_name,
-                'WarehousesShipping', (9, 'C'), (19, 'F'), (1, 1))
+    # load initial number of stationary wagons and locomotives at each node
+    M_init = faux.read_par_from_excel(Excel_file,'2DPar_M0', (2, 'A'), (10, 'C'), 2)
+    WS_init = faux.read_par_from_excel(Excel_file,'2DPar_lw0', (2, 'A'), (10, 'C'), 2)    
 
-    # Load the parameters for availability scenarios
-    f = faux.read_par_from_excel(file_name,
-                'ScenarioAvailability', (15, 'D'), (16, 'I'), (0, 1))
+    # Load cost information
+    FC = faux.read_par_from_excel(Excel_file,'3DPar_FC', (2, 'A'), (34, 'D'), 3)
+    FC = {key:val for key, val in FC.items() if val != 0}
+    VC = faux.read_par_from_excel(Excel_file,'3DPar_VC', (2, 'A'), (34, 'D'), 3)
+    VC = {key:val for key, val in VC.items() if val != 0}
 
-    phi = faux.read_par_from_excel(file_name,
-                'ScenarioAvailability', (19, 'D'), (30, 'J'), (1, 1))
+    # Load Delivery time information
+    tau = faux.read_par_from_excel(Excel_file,'3DPar_Tau', (2, 'A'), (4, 'D'), 3)
 
+    # Load Delivery time information
+    S = faux.read_par_from_excel(Excel_file,'5DPar_S', (2, 'A'), (100, 'F'), 5)
 
-    # Load the parameters for sales scenarios
-    D = faux.read_par_from_excel(file_name,
-                'ScenarioSales', (7, 'D'), (67, 'K'), (2, 1))
+    fixed_var = faux.ParaFixedInput(tau, H, OL, OW, NC, NL, NW)
 
-    SP = faux.read_par_from_excel(file_name,
-                'ScenarioSales', (74, 'D'), (134, 'K'), (2, 1))
-
-    SO = faux.read_par_from_excel(file_name,
-                'ScenarioSales', (137, 'D'), (145, 'J'), (1, 1))
-
-    # Loead the parameters for purchases scenarios
-    PC = faux.read_par_from_excel(file_name,
-                'ScenarioPurchases', (26, 'D'), (34, 'J'), (1, 1))
-
-    IC_ini_level = faux.read_par_from_excel(file_name,
-                'ScenarioPurchases', (38, 'D'), (39, 'K'), (0, 1))
-
-
-    # initialise HC
-    HCYES = faux.read_par_from_excel(file_name,
-                'set', (25, 'E'), (28, 'K'), (1, 1))
-
-    HC = {keys : 1 if values == 'yes' else 0
-          for keys, values in HCYES.items()}
-
-    # Initialise other parameters
-    IC_low = {
-    material : 0 for material in m
-    }
-    S_ini_level = {
-    material : 0 for material in m
-    }
-
-    fixed_var = faux.ParaFixedInput(p_min, p_max, PR, tao, miu,
-                                    n, LT, IC_low, IH_low, IC_upper, HC)
-
-    variable_par = faux.ParaVarInput(f, phi, D, SP, SO, PC, OC, OP,
-                                     IC_ini_level, S_ini_level)
+    variable_par = faux.ParaVarInput(FC, VC, P, WMAX, S, M_init, WS_init)
 
     return set_input, fixed_var, variable_par
+
+
 
 
 def main():
@@ -101,10 +76,10 @@ def main():
     optimisation model
     """
     # initialise the concreteModel
-    PSE_model = ConcreteModel()
+    RSO_model = ConcreteModel()
 
     # get the data input as objects
-    Excel_file = 'Borouge_Data_Scott_Demo.xlsx'
+    Excel_file = 'Pyomo_RSO_Parameter_Input.xlsx'
     set_input, fixed_par, variable_par = data_construction(Excel_file)
     #print(fixed_par.IH_low)
     #print(variable_par.SP)
